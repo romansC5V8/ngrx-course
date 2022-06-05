10367ac033e8583b91ef5da521fe159e2accc572
Santiago <sanandres_23@yahoo.com>
2022-06-05 02:48:13 +0200

    Login finished

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,24 @@
+import { createReducer, on } from '@ngrx/store';
+
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
+
+export const authFeatureKey = 'auth';
+
+export interface AuthState {
+  user: User
+}
+
+export const initialAuthState: AuthState = {
+  user: undefined
+}
+
+export const authReducer = createReducer(
+  initialAuthState,
+
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    }
+  })
+);
